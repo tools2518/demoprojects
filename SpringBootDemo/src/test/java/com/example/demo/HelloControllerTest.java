3cd29667302fde8abda5c9c28f5e898c85cf86aa
soumyabanerjee <soumyabanerjee@localhost>
2021-05-09 21:09:15 +0530

    Added test + actuator

--- SpringBootDemo/src/test/java/com/example/demo/HelloControllerTest.java
+++ SpringBootDemo/src/test/java/com/example/demo/HelloControllerTest.java
@@ -15,7 +15,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class SpringBootDemoApplicationTests {
+public class HelloControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
